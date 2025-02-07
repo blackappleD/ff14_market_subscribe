630dd5c5d18a8c649d1bbc7fa4052cd852d72e16
chent <aChen1874@163.com>
2025-02-07 12:25:18 +0800

    fix：限制universalis接口调用并发数

--- ff14_market_subscribe_backend/src/main/java/com/ff14/market/config/ThreadPoolExecutorConfig.java
+++ ff14_market_subscribe_backend/src/main/java/com/ff14/market/config/ThreadPoolExecutorConfig.java
@@ -19,7 +19,7 @@ public class ThreadPoolExecutorConfig {
 
 	@Bean
 	public ThreadPoolExecutor threadPoolExecutor() {
-		return new ThreadPoolExecutor(7, 7,
+		return new ThreadPoolExecutor(5, 5,
 				1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new Sleep5sResubmitHandler());
 	}
 
